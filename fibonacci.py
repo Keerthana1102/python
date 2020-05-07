0a7bc9f905125904c6569cde7b58cc69e6139b08
Keerthana1102 <55576232+Keerthana1102@users.noreply.github.com>
2020-05-07 20:57:15 +0530

    fibonacci series of n numbers

--- fibonacci.py
+++ fibonacci.py
@@ -1,7 +1,7 @@
 # code for fibonacci sequence of n numbers
 n = int(input("Enter the number of elements: "))
 n1 = 0
-n2 = 1;
+n2 = 1
 
 print(n1, end = ", ")
 print(n2, end = "")
@@ -11,4 +11,5 @@ for i in range(2, n):
   n3 = n1 + n2
   print(", ", n3, end = "")
   n1 = n2
-  n2 = n3
+  n2 = n3 
+  
