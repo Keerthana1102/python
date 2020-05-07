4a52652e5bb2c2f1909e19d238498827d1d861ed
Keerthana1102 <55576232+Keerthana1102@users.noreply.github.com>
2020-05-07 20:54:55 +0530

    Create fibonacci.py

---
+++ fibonacci.py
@@ -0,0 +1,14 @@
+# code for fibonacci sequence of n numbers
+n = int(input("Enter the number of elements: "))
+n1 = 0
+n2 = 1;
+
+print(n1, end = ", ")
+print(n2, end = "")
+
+i = 2
+for i in range(2, n):
+  n3 = n1 + n2
+  print(", ", n3, end = "")
+  n1 = n2
+  n2 = n3
